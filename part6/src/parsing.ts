130a07966950bf0ca5bb2b4a47526867d489bce8
Markus Wahlman <wahlman2002@gmail.com>
2025-03-11 22:19:24 +0200

    Add parsing for board

--- part6/src/parsing.ts
+++ part6/src/parsing.ts
@@ -1,57 +1,110 @@
-type RleData = {
-  headerLine: string;
-  headerLineX?: number;
-  headerLineY?: number;
-  headerLineRule?: string;
+type GameBoard = {
+  width: number;
+  height: number;
+  rule: string;
+  board: boolean[][];
 };
 
-export function rleParser(dataString: string): RleData {
-  const lines = dataString.split("\n");
+function parseHeaderLine(headerLine: string, gameBoard: GameBoard): GameBoard {
+  if (headerLine.length <= 0) {
+    throw new Error("Empty header line");
+  }
 
-  let rleData: RleData = {
-    headerLine: "",
-  };
+  const variables = headerLine.split(",");
 
-  for (let i = 0; i < lines.length; i++) {
-    lines[i] = lines[i].trim();
-
-    if (lines[i][0] === "#") {
-      continue;
+  for (let j = 0; j < variables.length; j++) {
+    const varValue = variables[j].split("=");
+    if (varValue.length !== 2) {
+      throw new Error("Invalid header line");
     }
+    const leftSide = varValue[0].trim();
+    const rightSide = varValue[1].trim();
 
-    if (rleData.headerLine === "") {
-      if (lines[i].length <= 0) {
-        throw new Error("Empty header line");
+    if (leftSide === "x") {
+      if (isNaN(Number(rightSide))) {
+        throw new Error("Variable x should be a number");
       }
-
-      const variables = lines[i].split(",");
-
-      for (let j = 0; j < variables.length; j++) {
-        const varValue = variables[j].split("=");
-        if (varValue.length != 2) {
-          throw new Error("Invalid header line");
-        }
-        const leftSide = varValue[0].trim();
-        const rightSide = varValue[1].trim();
-
-        if (leftSide === "x") {
-          if (isNaN(Number(rightSide))) {
-            throw new Error("Variable x should be a number");
-          }
-        } else if (leftSide === "y") {
-          if (isNaN(Number(rightSide))) {
-            throw new Error("Variable y should be a number");
-          }
-        } else if (leftSide === "rule") {
-          //save rule?
-        } else {
-          throw new Error("Invalid header line variable name");
-        }
+      gameBoard.width = Number(rightSide);
+    } else if (leftSide === "y") {
+      if (isNaN(Number(rightSide))) {
+        throw new Error("Variable y should be a number");
       }
-
-      rleData.headerLine = lines[i];
+      gameBoard.height = Number(rightSide);
+    } else if (leftSide === "rule") {
+      /*B3/S23 stands for Game of Life rules*/
+      if (rightSide !== "B3/S23") {
+        throw new Error("Invalid rule");
+      }
+      gameBoard.rule = rightSide;
+    } else {
+      throw new Error("Invalid header line variable name");
     }
   }
 
-  return rleData;
+  return gameBoard;
+}
+
+export function rleParser(dataString: string): GameBoard {
+  const lines = dataString.split("\n");
+
+  let gameBoard: GameBoard = {
+    width: 0,
+    height: 0,
+    rule: "",
+    board: [],
+  };
+
+  let curIndex;
+  for (curIndex = 0; curIndex < lines.length; curIndex++) {
+    lines[curIndex] = lines[curIndex].trim();
+
+    if (lines[curIndex][0] === "#") {
+      continue;
+    }
+
+    gameBoard = parseHeaderLine(lines[curIndex], gameBoard);
+    break;
+  }
+
+  if (!gameBoard.width || !gameBoard.height) {
+    throw new Error("Insufficient header line");
+  }
+
+  let pattern = "";
+  for (curIndex = curIndex + 1; curIndex < lines.length; curIndex++) {
+    if (lines[curIndex].search("!") > 0) {
+      pattern += lines[curIndex].split("!")[0];
+      break;
+    }
+
+    pattern += lines[curIndex];
+  }
+  pattern = pattern.trim();
+
+  const patternLines = pattern.split("$");
+
+  for (let i = 0; i < patternLines.length; ++i) {
+    let currentRow: boolean[] = [];
+
+    let runCount = 1;
+    for (const char of patternLines[i]) {
+      if (!isNaN(Number(char))) {
+        runCount = Number(char);
+        continue;
+      }
+
+      if (char === "o") {
+        currentRow.push(...Array(runCount).fill(true));
+        runCount = 1;
+      } else if (char === "b") {
+        currentRow.push(...Array(runCount).fill(false));
+        runCount = 1;
+      } else {
+        throw new Error("Invalid pattern character");
+      }
+    }
+    gameBoard.board.push(currentRow);
+  }
+
+  return gameBoard;
 }
--- part6/test/parsing.test.ts
+++ part6/test/parsing.test.ts
@@ -8,7 +8,8 @@ describe("RLE Parser", () => {
 
     const parsedData = rleParser(data);
 
-    expect(parsedData.headerLine).to.equal("x = 1, y = 2");
+    expect(parsedData.width).to.equal(1);
+    expect(parsedData.height).to.equal(2);
   });
 
   test("Ignores comments", () => {
@@ -18,7 +19,8 @@ describe("RLE Parser", () => {
     x = 1, y = 2`;
     const parsedData = rleParser(data);
 
-    expect(parsedData.headerLine).to.equal("x = 1, y = 2");
+    expect(parsedData.width).to.equal(1);
+    expect(parsedData.height).to.equal(2);
   });
 
   test("Throws error for empty data string", () => {
@@ -47,7 +49,145 @@ describe("RLE Parser", () => {
     expect(error).to.deep.equal(new Error("Invalid header line"));
   });
 
-  //Throws error for non numeric x or y
-  //Throws error for variables in header line that are not recognized
-  //
+  test("Throws error for non numeric x", () => {
+    const data = "x = q, y = 5";
+    let error;
+
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Variable x should be a number"));
+  });
+
+  test("Throws error for non numeric y", () => {
+    const data = "x = 5, y = q";
+    let error;
+
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Variable y should be a number"));
+  });
+
+  test("Throws error for not recognized variables", () => {
+    const data = "x = 5, y = 5, q = 5";
+    let error;
+
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Invalid header line variable name"));
+  });
+
+  test("Works with rules", () => {
+    const data = "x = 1, y = 2, rule = B3/S23";
+
+    const parsedData = rleParser(data);
+
+    expect(parsedData.width).to.equal(1);
+    expect(parsedData.height).to.equal(2);
+    expect(parsedData.rule).to.equal("B3/S23");
+  });
+
+  test("Throws error for invalid rule", () => {
+    const data = "x = 1, y = 2, rule = B36/S23";
+
+    let error;
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Invalid rule"));
+  });
+
+  test("Throws error for insufficient header line when y is missing", () => {
+    const data = "x = 1, rule = B3/S23";
+
+    let error;
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Insufficient header line"));
+  });
+
+  test("Throws error for insufficient header line when x is missing", () => {
+    const data = "y = 1, rule = B3/S23";
+
+    let error;
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Insufficient header line"));
+  });
+
+  test("Works with blinker pattern", () => {
+    const data = `x = 3, y = 1, rule = B3/S23
+      3o!`;
+
+    const parsedData = rleParser(data);
+
+    expect(parsedData.width).to.equal(3);
+    expect(parsedData.height).to.equal(1);
+    expect(parsedData.rule).to.equal("B3/S23");
+    expect(parsedData.board).to.deep.equal([[true, true, true]]);
+  });
+
+  test("Works with glider pattern", () => {
+    const data = `x = 3, y = 3, rule = B3/S23
+      bob$2bo$3o!`;
+
+    const parsedData = rleParser(data);
+
+    expect(parsedData.width).to.equal(3);
+    expect(parsedData.height).to.equal(3);
+    expect(parsedData.rule).to.equal("B3/S23");
+    expect(parsedData.board).to.deep.equal([
+      [false, true, false],
+      [false, false, true],
+      [true, true, true],
+    ]);
+  });
+
+  test("Throws error for invalid pattern character", () => {
+    const data = `x = 3, y = 3, rule = B3/S23
+      boz$2bo$3o!`;
+
+    let error;
+    try {
+      rleParser(data);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).to.deep.equal(new Error("Invalid pattern character"));
+  });
+
+  //Throws error for too long pattern
+
+  //Doesn't go out of bouds if we the pattern is longer than the width
+
+  //Works with blinker pattern?
+
+  //Works with glider pattern?
+
+  //Works with block pattern?
+
+  //Throws error for pattern without end
 });
